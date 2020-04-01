4b8f41e2dfbaadb5ea1056fdf98ae4b80bcb0a03
Runqing Fang <Kate@MacBook-Air-2.local>
2020-03-31 22:50:51 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,23 +2,21 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("user flipped " + cardsInPlay[0]);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipper " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[2]) {
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert ("You found a match!");
   }
   else {
-    alert ("Sorry, try again."); 
+    alert ("Sorry, try again.");
   }
 }
+
+function flipCard(cardID) {
+  if (cardsInPlay.length === 2) {
+    checkForMatch(); 
+  }
+  console.log("User flipped " + cards[cardID]);
+}
+
+flipCard(0);
+flipCard(2);
